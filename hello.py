5fe2e8e5d684190424be243747e6d400cf527ad9
David Handy <david@crunch.io>
2024-11-21 13:50:20 -0500

    Add hello.py so there is a Python file

---
+++ hello.py
@@ -0,0 +1,12 @@
+"""
+Example script for testing github actions with Python code
+"""
+import sys
+
+
+def main():
+    print("Hello from hello-github-actions!")
+
+
+if __name__ == '__main__':
+    sys.exit(main())
